1c0d6200b5b3a35ff687e582d4a12a8f95162f86
Gokturk Kose <gokturkbrk@gmail.com>
2023-08-15 13:37:39 +0200

    update necessary dependencies

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,8 +1,9 @@
 module.exports = {
     devServer: {
-      disableHostCheck: true,
-      port: 8080,
-      public: '0.0.0.0:8080'
+      client:{
+        webSocketURL:'ws://0.0.0.0:8080/ws'
+      },
+      port: 8080
     },
     publicPath: "/",
     pages: {
